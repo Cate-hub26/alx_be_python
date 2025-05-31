395ed169b310abbf6e486bfd145e1972892f9f61
Cate <catherinenzisa2017@gmail.com>
2025-05-31 15:51:18 +0300

    Add match_case_calculator.py file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-operator = input("Choose your operation (+, -, *, /): ").strip()
+operator = input("Choose your operation (+,-,*,/): ").strip()
 
 match operator:
     case "+":
