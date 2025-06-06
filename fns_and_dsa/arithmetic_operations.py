18c5e016bedd2903e7af024c226c9dd270f7b725
Cate <catherinenzisa2017@gmail.com>
2025-06-06 13:24:47 +0300

    Update arithmetics_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,10 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
-        result = num1 + num2
-        return result
+        return num1 + num2
     elif operation == "subtract":
-        result = num1 - num2
-        return result
+        return num1 - num2
     elif operation == "multiply":
-        result = num1 * num2
-        return result
+        return num1 * num2
     elif operation == "divide" :
         if num2 == 0:
             return "Error: Division by zero"
