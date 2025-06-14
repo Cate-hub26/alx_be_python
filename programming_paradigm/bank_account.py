e55d0bd6f5a4a30b447ac214788a5cbf97255093
Cate <catherinenzisa2017@gmail.com>
2025-06-14 15:12:36 +0300

    Add bank_account.py file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,14 +5,14 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
-            print(f"Deposited: ${amount}")
+            print(f"Deposited: ${amount:.2f}")
         else:
             print("Invalid deposit amount.")
     
     def withdraw(self, amount):
         if amount > 0 and amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount}")
+            print(f"Withdrew: ${amount:.2f}")
             return True
         else:
             print("Insufficient funds.")
@@ -23,6 +23,6 @@ class BankAccount:
         return f"Current Balance: ${self.account_balance}."
        
 bankaccount = BankAccount(10000.00)
-bankaccount.deposit(2000.0)
-bankaccount.withdraw(230.0)
+bankaccount.deposit(2000.00)
+bankaccount.withdraw(230.00)
 bankaccount.display_balance()
