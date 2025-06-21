d765e54412539c5b13e8798fd4f5c97bd2621158
Cate <catherinenzisa2017@gmail.com>
2025-06-21 13:10:27 +0300

    Update library_system.py file

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,10 +2,16 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+        
+    def __str__(self):
+        return f"{self.title} by {self.name}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author, file_size)
+        
+    def __str__(self):
+        return f"{self.title} by {self.name}, File Size: {self.file_size}"
         
     #def __init__(self, file_size):
         #self.file_size = file_size
@@ -13,6 +19,9 @@ class EBook(Book):
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author, page_count)
+        
+    def __str__(self):
+        return f"{self.title} by {self.name}, File Size: {self.file_size}"
         
 class Library:
     def __init__(self, books):
