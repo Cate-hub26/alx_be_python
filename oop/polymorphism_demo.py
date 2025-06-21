9061c15ecacbda0f1d4b100e285e71a712ea4993
Cate <catherinenzisa2017@gmail.com>
2025-06-21 14:56:19 +0300

    Add polymorphism_demo.py file

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,22 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        area = self.length * self.width
+        return area
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        area = math.pi * self.radius * self.radius
+        return area
+    
+
+        
+        
